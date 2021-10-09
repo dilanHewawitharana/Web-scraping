38dd0a8ce896b99e9786cfa0175eff8cb1b62432
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2021-10-09 09:25:18 +0900

    finished python web scaping code

---
+++ main.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
+    
+    soup = BeautifulSoup(content, 'lxml')
+
+    # courses_html_tag = soup.find_all('h5')
+    # for course in courses_html_tag:
+    #     print(course.text)
+
+    course_cards = soup.find_all('div', class_='card')
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+
+        print(f'{course_name} costs {course_price}')
+
+
